417ac662be5c3e7d37d842bac9d66498fa9a3c94
Yugal Teotia <67860100+yugalteotia@users.noreply.github.com>
2024-03-26 07:12:53 +0530

    update class13S1.sh

--- class13S1.sh
+++ class13S1.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+finished=0
+
 echo "Choose your favourite day of the week: "
 
-echo "1- monday"
-echo "2- tuesday"
-echo "3- wednesday"
-echo "4- thursdady"
-echo "5- friday"
-echo "6- saturday"
-echo "7- sunday"
+while [ $finished -ne 1 ]
+do
+	echo "1- monday"
+	echo "2- tuesday"
+	echo "3- wednesday"
+	echo "4- thursdady"
+	echo "5- friday"
+	echo "6- saturday"
+	echo "7- sunday"
+	echo "press 0 to exit the script!!"
 
 read distro;
 
 case $distro in
+	0) finished=1;;
 	1) echo "its monday";;
 	2) echo "its tuesday";;
 	3) echo "its wednesday";;
@@ -20,5 +26,6 @@ case $distro in
 	5) echo "its friday";;
 	6) echo "its saturday";;
 	7) echo "its sunday";;
-	*) echo "its not a choice i gave you to choose from :(";;
+	*) echo "its not a choice i gave you to choose from :("
 esac
+done
